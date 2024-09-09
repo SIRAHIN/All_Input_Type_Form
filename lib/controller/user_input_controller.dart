6f5838782a6df42900cb05c8c7b58f655cf8584c
unknown <sirahin30@gmail.com>
2024-09-09 15:29:25 +0600

    Date and time input section Added

--- lib/controller/user_input_controller.dart
+++ lib/controller/user_input_controller.dart
@@ -1,15 +1,18 @@
 import 'package:all_type_form_inpuut/model/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class UserInputController extends GetConnect {
   final userFormData = <UserModel>[].obs;
-  
 
   // For Text Controller Section //
   TextEditingController userNameEditiongController = TextEditingController();
   TextEditingController userPhoneEditingController = TextEditingController();
-  
+
+  // For Text Controller Section For Date And Time //
+  TextEditingController datePickerController = TextEditingController();
+  TextEditingController timePickerController = TextEditingController();
 
   // For Check Box Section //
   final isGraphicsDesigner = false.obs;
@@ -32,29 +35,28 @@ class UserInputController extends GetConnect {
   void toggleGameDesginer() {
     isGameDesigner.value = !isGameDesigner.value;
   }
-  
 
   // For Drop Down Section //
   final experience =
       ['Experience 0-1', 'Experience 2+', 'Experience 3+', 'Experience 4+'].obs;
   final initialExp = 'Experience 0-1'.obs;
-  
+
   // For Multiple toggle Option Section //
   final toggleValue = [false, false, false, false].obs;
-  
+
   // for Switch Button //
   final isVerificationOn = false.obs;
   void toggleVerificatonOn() {
     isVerificationOn.value = !isVerificationOn.value;
   }
-  
+
   // for Radio Button //
   final initalLevel = 'Beginner'.obs;
   void toggleLevel(passValue) {
     initalLevel.value = passValue;
   }
- 
- // on submit form func //
+
+  // on submit form func //
   onSubmitForm() {
     userFormData.add(UserModel(
         userName: userNameEditiongController.text,
@@ -67,6 +69,48 @@ class UserInputController extends GetConnect {
         expLevel: initalLevel.value,
         needNotification: isVerificationOn.value,
         osType: toggleValue));
+  }
+  
+
+  // Func For Selecting The Data //
+  void selectDate(context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    // Checking Condition That Picked Null or Not //
+    if (pickedDate != null) {
+      var formatter = DateFormat.yMd();
+      String formattedDate = formatter.format(pickedDate);
+      datePickerController.text = formattedDate;
+    }
+  }
+  
+  // Func For Selecting The Time //
+  void selecteTime(context) async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (pickedTime != null) {
+      // Convert TimeOfDay to DateTime
+      DateTime now = DateTime.now();
+      DateTime pickedDateTime = DateTime(
+        now.year,
+        now.month,
+        now.day,
+        pickedTime.hour,
+        pickedTime.minute,
+      );
+
+      // Format DateTime
+      DateFormat formatter = DateFormat.jm();
+      String formattedTime = formatter.format(pickedDateTime);
+      timePickerController.text = formattedTime;
+    }
   }
 
   @override
--- lib/view/user_input_form.dart
+++ lib/view/user_input_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class UserInputForm extends GetView<UserInputController> {
   const UserInputForm({super.key});
@@ -17,196 +18,227 @@ class UserInputForm extends GetView<UserInputController> {
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 830,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Text(
-                      "User Form",
-                      style: TextStyle(fontSize: 30, color: Colors.deepPurple),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Center(
+                  child: Text(
+                    "User Form",
+                    style: TextStyle(fontSize: 30, color: Colors.deepPurple),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: controller.userNameEditiongController,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      label: Text('Enter Your Name')),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: controller.userPhoneEditingController,
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.phone),
+                    label: Text('Enter Your Phone Number'),
+                    prefix: Text('+8801 '),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text('Please Select You Skills'),
+                Obx(
+                  () => CheckboxMenuButton(
+                      value: controller.isGraphicsDesigner.value,
+                      onChanged: (value) {
+                        controller.toggleGraphicsDesginer();
+                      },
+                      child: const Text('Graphics Design')),
+                ),
+                Obx(
+                  () => CheckboxMenuButton(
+                      value: controller.isWebDesigner.value,
+                      onChanged: (value) {
+                        controller.toggleWebDesginer();
+                      },
+                      child: const Text('Web Design')),
+                ),
+                Obx(
+                  () => CheckboxMenuButton(
+                      value: controller.isAppDesigner.value,
+                      onChanged: (value) {
+                        controller.toggleAppDesginer();
+                      },
+                      child: const Text('App Design')),
+                ),
+                Obx(
+                  () => CheckboxMenuButton(
+                      value: controller.isGameDesigner.value,
+                      onChanged: (value) {
+                        controller.toggleGameDesginer();
+                      },
+                      child: const Text('Game Design')),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text('Please Select You Experience Level'),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Obx(() {
+                    return DropdownButton<String>(
+                      items: controller.experience
+                          .map(
+                            (e) => DropdownMenuItem<String>(
+                              value: e,
+                              child: Text(e),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        if (value != null) {
+                          controller.initialExp.value = value;
+                        }
+                      },
+                      value: controller.initialExp.value,
+                    );
+                  }),
+                ),
+                const Text('Please Select You Expertise Level'),
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        Obx(() => Radio<String>(
+                              value: 'Beginner', // Value of this Radio button
+                              groupValue: controller.initalLevel
+                                  .value, // Observable that stores the selected value
+                              onChanged: (value) {
+                                if (value != null) {
+                                  controller.toggleLevel(
+                                      value); // Update the observable with the selected value
+                                 
+                                }
+                              },
+                            )),
+                        const Text('Beginner'),
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: controller.userNameEditiongController,
-                    decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.person),
-                        label: Text('Enter Your Name')),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: controller.userPhoneEditingController,
-                    decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.phone),
-                      label: Text('Enter Your Phone Number'),
-                      prefix: Text('+8801 '),
+                    Row(
+                      children: [
+                        Obx(() => Radio<String>(
+                              value: 'Advanced', // Value of this Radio button
+                              groupValue: controller.initalLevel
+                                  .value, // Observable that stores the selected value
+                              onChanged: (value) {
+                                if (value != null) {
+                                  controller.toggleLevel(
+                                      value);
+                                     // Update the observable with the selected value
+                                }
+                              },
+                            )),
+                        const Text('Advanced'),
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text('Please Select You Skills'),
-                  Obx(
-                    () => CheckboxMenuButton(
-                        value: controller.isGraphicsDesigner.value,
-                        onChanged: (value) {
-                          controller.toggleGraphicsDesginer();
-                        },
-                        child: const Text('Graphics Design')),
-                  ),
-                  Obx(
-                    () => CheckboxMenuButton(
-                        value: controller.isWebDesigner.value,
-                        onChanged: (value) {
-                          controller.toggleWebDesginer();
-                        },
-                        child: const Text('Web Design')),
-                  ),
-                  Obx(
-                    () => CheckboxMenuButton(
-                        value: controller.isAppDesigner.value,
-                        onChanged: (value) {
-                          controller.toggleAppDesginer();
-                        },
-                        child: const Text('App Design')),
-                  ),
-                  Obx(
-                    () => CheckboxMenuButton(
-                        value: controller.isGameDesigner.value,
-                        onChanged: (value) {
-                          controller.toggleGameDesginer();
-                        },
-                        child: const Text('Game Design')),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text('Please Select You Experience Level'),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Obx(() {
-                      return DropdownButton<String>(
-                        items: controller.experience
-                            .map(
-                              (e) => DropdownMenuItem<String>(
-                                value: e,
-                                child: Text(e),
-                              ),
-                            )
-                            .toList(),
-                        onChanged: (value) {
-                          if (value != null) {
-                            controller.initialExp.value = value;
-                          }
-                        },
-                        value: controller.initialExp.value,
-                      );
-                    }),
-                  ),
-                  Text('Please Select You Expertise Level'),
-                  Row(
-                    children: [
-                      Row(
-                        children: [
-                          Obx(() => Radio<String>(
-                                value: 'Beginner', // Value of this Radio button
-                                groupValue: controller.initalLevel
-                                    .value, // Observable that stores the selected value
-                                onChanged: (value) {
-                                  if (value != null) {
-                                    controller.toggleLevel(
-                                        value); // Update the observable with the selected value
-                                   
-                                  }
-                                },
-                              )),
-                          const Text('Beginner'),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Obx(() => Radio<String>(
-                                value: 'Advanced', // Value of this Radio button
-                                groupValue: controller.initalLevel
-                                    .value, // Observable that stores the selected value
-                                onChanged: (value) {
-                                  if (value != null) {
-                                    controller.toggleLevel(
-                                        value);
-                                       // Update the observable with the selected value
-                                  }
-                                },
-                              )),
-                          const Text('Advanced'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      const Text('Do you want to Enable the Notification'),
-                      Transform.scale(
-                        scale: 0.5,
-                        child: Obx(
-                          () => Switch(
-                            value: controller.isVerificationOn.value,
-                            onChanged: (bool value1) {
-                              controller.toggleVerificatonOn();
-                            },
-                          ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    const Text('Do you want to Enable the Notification'),
+                    Transform.scale(
+                      scale: 0.5,
+                      child: Obx(
+                        () => Switch(
+                          value: controller.isVerificationOn.value,
+                          onChanged: (bool value1) {
+                            controller.toggleVerificatonOn();
+                          },
                         ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text('Please Select you OS'),
-                  Obx(
-                    () {
-                      return ToggleButtons(
-                          onPressed: (index) {
-                            controller.toggleValue[index] =
-                                !controller.toggleValue[index];
-                          },
-                          children: [
-                            Icon(FontAwesomeIcons.apple),
-                            Icon(FontAwesomeIcons.windows),
-                            Icon(FontAwesomeIcons.linux),
-                            Icon(FontAwesomeIcons.android),
-                          ],
-                          isSelected: controller.toggleValue);
-                    },
-                  ),
-
-                  const SizedBox(height: 10,),
-
-                  Center(
-                    child: SizedBox(
-                    width: 180,
-                      child: MaterialButton(
-                      color: Colors.teal,
-                      child: Text('Submit', style: TextStyle(
-                       fontSize: 20,
-                       color: Colors.white
-                      ),),
-                      onPressed: () {
-                        controller.onSubmitForm();
-                      },),
                     ),
-                  )
-                ],
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+            
+                const Text('Please Select you OS'),
+                Obx(
+                  () {
+                    return ToggleButtons(
+                        onPressed: (index) {
+                          controller.toggleValue[index] =
+                              !controller.toggleValue[index];
+                        },
+                        isSelected: controller.toggleValue,
+                        children: [
+                          Icon(FontAwesomeIcons.apple),
+                          Icon(FontAwesomeIcons.windows),
+                          Icon(FontAwesomeIcons.linux),
+                          Icon(FontAwesomeIcons.android),
+                        ]);
+                  },
+                ),
+            
+                Column(
+                mainAxisSize: MainAxisSize.min,
+          children: [
+          // Select Date Section // 
+            TextField(
+              controller: controller.datePickerController,
+              decoration: const InputDecoration(
+                labelText: 'Select Date',
+                suffixIcon: Icon(Icons.calendar_today),
               ),
+              readOnly: true,
+              onTap: ()  {
+               controller.selectDate(context);
+              },
+            ),
+            
+          const SizedBox(height: 16.0),
+          
+           // Select Time Section // 
+          TextField(
+          controller: controller.timePickerController,
+          decoration: const InputDecoration(
+            labelText: 'Select Time',
+            suffixIcon: Icon(Icons.access_time),
+          ),
+          readOnly: true,
+          onTap: ()  {
+            controller.selecteTime(context);
+          },
+        ),
+          ],
+        ),
+            
+                const SizedBox(height: 10,),
+            
+                Center(
+                  child: SizedBox(
+                  width: 180,
+                    child: MaterialButton(
+                    color: Colors.teal,
+                    child: const Text('Submit', style: TextStyle(
+                     fontSize: 20,
+                     color: Colors.white
+                    ),),
+                    onPressed: () {
+                      controller.onSubmitForm();
+                    },),
+                  ),
+                )
+              ],
             ),
           ),
         ),
